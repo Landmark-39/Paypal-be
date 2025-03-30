15c8abcf5a96f2b1a3b7cfb1ae89df8046871c38
nwa1990 <nwachukwuchideraa@yahoo.com>
2025-03-30 01:58:10 +0000

    1st commit

---
+++ transcript.sh
@@ -0,0 +1,10 @@
+echo "welcome to the chemistry department, the university of ibadan trascript portal"
+echo "please enter your name"
+read "name"
+echo "please enter you current CGPA"
+read "CGPA"
+if [ $CGPA -gt 2 ]
+then
+echo "congratulations"
+fi
+
